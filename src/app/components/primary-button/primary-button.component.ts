3505a34c8843648a8967af7d67c33ee3c5065fe1
Srivandhi <srivandhi264@gmail.com>
2025-02-08 01:01:50 +0530

    added duplicate item values

---
+++ src/app/components/primary-button/primary-button.component.ts
@@ -0,0 +1,20 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  template: `
+   <button class="bg-pink-400 w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-85" (click) = "btnClicked.emit()">
+    {{label()}}
+   </button>
+
+  `,
+  styleUrl: './primary-button.component.css'
+})
+export class PrimaryButtonComponent {
+  label = input('');
+
+  btnClicked = output();
+
+ 
+}
